906cdc69b504e94077a34aeabca23f5fa0593daa
Jenish Galani <jenishgalani4543@gmail.com>
2025-01-09 12:35:05 +0530

    Update EmailStartupService.java
    .

--- src/main/java/com/example/Email_Send_Every_Minute/EmailSend/EmailStartupService.java
+++ src/main/java/com/example/Email_Send_Every_Minute/EmailSend/EmailStartupService.java
@@ -15,7 +15,7 @@ public class EmailStartupService {
     @Scheduled(fixedRate = 60000)
     @EventListener(ApplicationReadyEvent.class)
     public void sendEmailOnStartup() {
-        String to = "";
+        String to = ""; //The recipient's email address
         String subject = "One-Time Email";
         String text = "This is a one-time email sent on application startup.";
         emailService.sendEmail(to, subject, text);
